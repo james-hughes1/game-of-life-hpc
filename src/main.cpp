cce741d90653683cb7287813e3d278e53c7aaa57
James Hughes <jh2284@cam.ac.uk>
2024-03-10 23:15:23 +0000

    Build: Create basic CMake build rules with clang-format and doxygen.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * \file main.cpp Main program file.
+ */
+
+#include <iostream>
+
+int main() {
+    /**
+     * New docs.
+     */
+    std::cout << "This is a test." << std::endl;
+    return 0;
+}
