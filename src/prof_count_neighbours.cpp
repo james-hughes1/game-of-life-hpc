af6b233ce6ecda68222a1d497ec67e223b768f3a
James Hughes <jh2284@cam.ac.uk>
2024-03-13 23:46:06 +0000

    Perf: FIX; Re-write prof_count_neighbours.cpp to profile new separable convolution.

--- src/prof_count_neighbours.cpp
+++ src/prof_count_neighbours.cpp
@@ -11,24 +11,26 @@ int main() {
 
     Matrix A = matrix::generate_matrix(1000, 1000);
 
-    Matrix B(A.n_rows, A.n_cols);
+    int n_rows = A.n_rows;
+    int n_cols = A.n_cols;
 
-    for (int i = 0; i < A.n_rows; i++) {
-        for (int j = 0; j < A.n_cols; j++) {
-            B(i, j) = 0;
-            for (int p = -1; p < 2; p++) {
-                for (int q = -1; q < 2; q++) {
-                    B(i, j) += A(i + p, j + q);
-                }
-            }
-            B(i, j) -= A(i, j);
-            if (i == 0 or i == A.n_rows - 1 or j == 0 or j == A.n_cols - 1) {
-                B(i, j) = -1;
-            }
+    // Row convolution
+    Matrix B(n_rows, n_cols);
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 1; j < n_cols - 1; j++) {
+            B(i, j) = A(i, j - 1) + A(i, j) + A(i, j + 1);
         }
     }
 
-    std::cout << "Test entry: " << B(1, 1) << std::endl;
+    // Column convolution (and subtract 1 for the proper convolution value).
+    Matrix C(n_rows, n_cols);
+    for (int i = 1; i < n_rows - 1; i++) {
+        for (int j = 1; j < n_cols - 1; j++) {
+            C(i, j) = B(i - 1, j) + B(i, j) + B(i + 1, j) - A(i, j);
+        }
+    }
+
+    std::cout << "Test entry: " << C(1, 1) << std::endl;
 
     return 0;
 }
